0d4d1e45f37a6381241f66953a0419f6d88319fd
Jonathan Bahena <bahena@Jonathans-MacBook-Air.local>
2021-08-06 08:35:36 -0500

    finished the docs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,26 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
 
-  # GET /posts
   def index
     @posts = Post.all
-
-    render json: @posts
+   
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
   end
-
   # GET /posts/1
   def show
     render json: @post
   end
 
   # POST /posts
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      render json: @post, status: :created, location: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
+  def new
+    @post = Post.new
+   
+    respond_to do |format|
+      format.html  # new.html.erb
+      format.json  { render :json => @post }
     end
   end
 
